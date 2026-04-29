3e8f40fad0c1c0c62077668e2866fd1c93c85ae1
ThommyShelby9 <merluxsolitaire@gmail.com>
2026-04-29 09:24:45 +0100

    feat(brief): bump TOTAL_BRIEF_STEPS to 5 for inspection step

--- types/brief.ts
+++ types/brief.ts
@@ -2,7 +2,7 @@ export type {
   BriefInput,
 } from '~/server/utils/schemas/brief'
 
-export type BriefStep = 1 | 2 | 3 | 4
-export const TOTAL_BRIEF_STEPS = 4 as const
+export type BriefStep = 1 | 2 | 3 | 4 | 5
+export const TOTAL_BRIEF_STEPS = 5 as const
 
 export const BRIEF_LOCALSTORAGE_KEY = 'rostel_portfolio_brief_draft_v1'
